5664a44cb0afc51f92166b75fb8b5e0e3f6f54bd
Xingtian Ma <ma.xingtian04@gmail.com>
2024-08-21 14:17:55 -0400

    Oops

--- deploy-slash.js
+++ deploy-slash.js
@@ -1,7 +1,7 @@
 const { REST, Routes} = require("discord.js")
 const botID = "1274104343167111168";
 const serverID = "1254267128312365167";
-const botToken = "MTI3NDEwNDM0MzE2NzExMTE2OA.G9ZCVT.8YYKgZED1mLMisV1KqgxwQe0mcI6HlXUaD3uSU";
+const botToken = "";
 
 const rest = new REST().setToken(botToken);
 const slashRegister = async () => {
